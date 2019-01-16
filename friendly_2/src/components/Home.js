b82130af1d6f22dcae5dd376c0a7ec45da6ce999
github username <github email address>
2019-01-15 20:37:05 -0500

    sass files

--- friendly_2/src/components/Home.js
+++ friendly_2/src/components/Home.js
@@ -24,6 +24,70 @@ class Home extends React.Component {
               <a href="#" onClick={this.handleClick}className="btn btn--white btn--animated">Discover Friendship</a>
             </div>
             </header>
+            <section className="grid-test">
+            <div className="row">
+              <div className= 'col-1-of-2'>
+              Column 1 of 2
+              </div>
+              <div className= 'col-1-of-2'>
+              Column 1 of 2
+              </div>
+            </div>
+
+            <div className="row">
+              <div className= 'col-1-of-3'>
+              Column 1 of 3
+              </div>
+              <div className= 'col-1-of-3'>
+              Column 1 of 3
+              </div>
+              <div className= 'col-1-of-3'>
+              Column 1 of 3
+              </div>
+            </div>
+
+            <div className="row">
+              <div className= 'col-1-of-3'>
+              Column 1 of 3
+              </div>
+              <div className= 'col-2-of-3'>
+              Column 2 of 3
+              </div>
+            </div>
+
+            <div className="row">
+              <div className= 'col-1-of-4'>
+              Column 1 of 4
+              </div>
+              <div className= 'col-1-of-4'>
+              Column 1 of 4
+              </div>
+              <div className= 'col-1-of-4'>
+              Column 1 of 4
+              </div>
+            </div>
+
+            <div className="row">
+              <div className= 'col-1-of-4'>
+              Column 1 of 4
+              </div>
+              <div className= 'col-1-of-4'>
+              Column 1 of 4
+              </div>
+              <div className= 'col-2-of-4'>
+              Column 2 of 4
+              </div>
+            </div>
+
+            <div className="row">
+              <div className= 'col-1-of-4'>
+              Column 1 of 4
+              </div>
+              <div className= 'col-3-of-4'>
+              Column 3 of 4
+              </div>
+            </div>
+            </section>
         </div>
     )
   }
